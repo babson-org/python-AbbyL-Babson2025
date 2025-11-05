118143de838801be8b77459d51ce9aec19de1baf
Abby-babson2025 <abbygayle.liem@gmail.com>
2025-11-05 15:12:50 +0000

    Merge upstream/main, preserving student copies

--- sandbox/combine.py
+++ sandbox/combine.py
@@ -5,5 +5,13 @@ def combine(left, right):
 
     cnt = 0
 
-    while cnt < 50:
-        cnt += 1
+    while l_cur < len(left) and r_cur < len(right):
+        if left(l_cur) < right(r_cur):
+            array.append(left(l_cur))
+            l_cur += 1
+
+        else:
+            array.append(right(r_cur))
+            r_cur +=1
+
+    return array + left[l_cur:]+ right[r_cur:]
